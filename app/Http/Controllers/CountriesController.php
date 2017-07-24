c95f5e74ac812b0014b621b13c8197e0ff5db4b9
Tom Holland <codesman@hackattic.net>
2017-07-23 18:21:46 -0700

    Countries Index from DB

--- app/Http/Controllers/CountriesController.php
+++ app/Http/Controllers/CountriesController.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Country;
 use Illuminate\Http\Request;
 
 class CountriesController extends Controller
 {
     public function index()
     {
-        return
-            [
-                ['name' => 'United States', 'ref' => 'united-states'],
-                ['name' => 'Europe', 'ref' => 'europe'],
-                ['name' => 'South America', 'ref' => 'south-america'],
-                ['name' => 'Central America', 'ref' => 'central-america'],
-                ['name' => 'Caribbean', 'ref' => 'caribbean'],
-                ['name' => 'Africa', 'ref' => 'africa'],
-            ];
+        return Country::all();
     }
 }
