ac5a54df07b78bfa394455e035f300d37908e02d
Tom Holland <codesman@hackattic.net>
2017-07-24 23:42:12 -0700

    Add Unit Tests

---
+++ tests/Unit/CountryTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Country;
+use Tests\TestCase;
+
+class CountryTest extends TestCase
+{
+    protected $country;
+
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Instantiate a Country
+        if (is_null($this->country)) {
+            $this->country = new Country();
+        }
+    }
+    
+    /**
+     * Verify that the Country Class has the scopeRef method.
+     * 
+     * @return void
+     */
+    public function testScopeRef()
+    {
+        $this->assertTrue(
+            method_exists(Country::class, 'scopeRef'),
+            'Country does not have the scopeRef method'
+        );
+    }
+
+    /**
+     * Verify that the Country Class has the name attribute
+     * 
+     * @return void
+     */
+    public function testHasNameAttribute()
+    {
+        $this->assertObjectHasAttribute(
+            'name',
+            $this->country,
+            'Country does not have the name attribute'
+        );
+    }
+
+    /**
+     * Verify that the Country Class has the ref attribute
+     * 
+     * @return void
+     */
+    public function testHasRefAttribute()
+    {
+        $this->assertObjectHasAttribute(
+            'ref',
+            $this->country,
+            'Country does not have the ref attribute'
+        );
+    }
+}
